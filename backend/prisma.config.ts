ee82dedbef1698efab9538e046cf3e475437ca6d
dicky <dicky@example.com>
2026-05-30 20:11:41 +0700

    fix prisma config

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: 'postgresql://a@localhost:5432/umkm_grow_db?schema=public',
+    url: process.env.DATABASE_URL,
   },
 });
