13d8d4f2dc734ef20e14955fc6d9104dbac8717a
dicky <dicky@example.com>
2026-06-01 19:22:49 +0700

    fix prisma

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,22 +1,9 @@
-import {
-  Injectable,
-  OnModuleInit,
-  OnModuleDestroy,
-} from '@nestjs/common';
-
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-    console.log('✅ Prisma Connected');
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
   }
 }
